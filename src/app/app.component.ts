4e1fafec089c43539a7aea7b8334161dfce5dc73
Shahir Jalal <shahirjalalhazli@gmail.com>
2025-11-25 02:06:35 +0800

    Initial commit: Angular 18 + Tailwind TaskFlow UI with real backend integration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,41 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NgFor, NgIf } from '@angular/common';
+import { TaskService, Task } from './task.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [FormsModule, NgFor, NgIf, HttpClientModule],
+  providers: [TaskService],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'frontend';
+export class AppComponent implements OnInit {
+  tasks: Task[] = [];
+  newTask = '';
+
+  constructor(private taskService: TaskService) { }
+
+  ngOnInit() {
+    this.loadTasks();
+  }
+
+  loadTasks() {
+    this.taskService.getTasks().subscribe(tasks => this.tasks = tasks);
+  }
+
+  addTask() {
+    if (this.newTask.trim()) {
+      this.taskService.addTask(this.newTask.trim()).subscribe(() => {
+        this.newTask = '';
+        this.loadTasks();
+      });
+    }
+  }
+
+  deleteTask(id: number) {
+    this.taskService.deleteTask(id).subscribe(() => this.loadTasks());
+  }
 }
